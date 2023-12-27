e1a0e84df07420f863079dd1e4a4854acb5a2ea2
Marcus Brown <marcus.brown1234567@gmail.com>
2023-12-26 22:47:37 -0500

    added import feature using react drop-zone papaparse and toastify

--- frontend/src/pages/Import.js
+++ frontend/src/pages/Import.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CSVGenerator from '../components/CSVGenerator';
+import CSVImport from '../components/CSVImport';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
@@ -12,8 +12,12 @@ const Import = () => {
         <p>
           Click the button to upload your previous training data as a CSV file.
         </p>
-        <p>note: the CSV file must maintain its original formatting </p>
-        <CSVGenerator></CSVGenerator>
+        <p>
+          note: the CSV file must maintain its original formatting from{' '}
+          <a href="/Export">Export page</a> and will overwrite your apps
+          currently saved data
+        </p>
+        <CSVImport></CSVImport>
       </div>
       <Footer></Footer>
     </div>
