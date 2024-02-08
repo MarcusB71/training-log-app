41abb37bcb965591374631afe098ab9223610dff
Marcus Brown <marcus.brown1234567@gmail.com>
2024-02-07 21:24:06 -0500

    created login form component

---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,164 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://powertrackr.netlify.app/">
+        PowerTrackr
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// TODO remove, shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+  const notifyError = () => toast.error('Failed to log in');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log('formData:', {
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+
+    try {
+      //currently hard coded post
+      const response = await axios.post('http://localhost:8080/login', {
+        email,
+        password,
+      });
+      localStorage.setItem('userEmail', email);
+
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      window.location.href = '/';
+    } catch (error) {
+      notifyError();
+      console.log('Login failed: ', error.response.data.message);
+    }
+  };
+
+  return (
+    <div className="container">
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <div
+            className="login-container"
+            style={{
+              border: '1px solid black',
+              borderRadius: '2%',
+              boxShadow: '4px 5px 5px grey',
+              padding: '25px',
+              background: 'var(--light)',
+            }}
+          >
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 0,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: '' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Log in
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  style={{ background: 'rgb(248,249,250)' }}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  style={{ background: 'rgb(248,249,250)' }}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 1, mb: 2 }}
+                >
+                  Log In
+                </Button>
+                <Grid container>
+                  {/* <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid> */}
+                  <Grid item>
+                    <Link href="Signup" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+            <Copyright sx={{ mt: 2, mb: 1 }} />
+          </div>
+        </Container>
+      </ThemeProvider>
+      <ToastContainer></ToastContainer>
+    </div>
+  );
+}
