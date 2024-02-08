346690b6c6cad00055c00a423ad46a8dd20a1a01
Marcus Brown <marcus.brown1234567@gmail.com>
2024-02-07 21:54:40 -0500

    added userRoutes and mongo connection

--- server/server.js
+++ server/server.js
@@ -1,32 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const authMiddleware = require('./middleware/authMiddleware');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+
 const app = express();
 const HTTP_PORT = 8080;
 
+mongoose.connect(process.env.MONGOSTRING);
+
+//middleware
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../build')));
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
-app.get('/Export', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
-app.get('/Import', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
-app.get('/Logs', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
+// login and signup /post in userRoutes.js
+app.use('/', userRoutes);
+
 app.get('/api', (req, res) => {
   res.json({ users: ['user1', 'user2', 'user3'] });
 });
 
-const startServer = () => {
-  app.listen(HTTP_PORT, () => {
-    console.log(`app listening on: ${HTTP_PORT}`);
-  });
-};
-startServer();
+app.listen(HTTP_PORT, () => {
+  console.log(`app listening on: ${HTTP_PORT}`);
+});
