547c4042fbc7fa921214d4bd86577ef888a60330
Marcus Brown <marcus.brown1234567@gmail.com>
2024-02-07 21:46:05 -0500

    added login and signup pages to app routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import Logs from './pages/Logs';
 import Export from './pages/Export';
 import Import from './pages/Import';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -15,8 +17,8 @@ function App() {
           <Route path="/Logs" element={<Logs />}></Route>
           <Route path="/Export" element={<Export />}></Route>
           <Route path="/Import" element={<Import />}></Route>
-          {/* <Route path="/sample-programs" element={<SamplePrograms />}></Route>
-          <Route path="/your-logs" element={<YourLogs />}></Route> */}
+          <Route path="/Login" element={<Login />}></Route>
+          <Route path="/SignUp" element={<SignUp />}></Route>
         </Routes>
       </Router>
     </>
